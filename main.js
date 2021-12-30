80a079dc5e4468615bbea07943a8b7bf0198330e
Juan Carlos <ajcarlos032@gmail.com>
2021-12-30 12:17:24 -0500

    action click button

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ const $APP = document.getElementById('app')
 
 const input = ({ type, value, id, placeholder }) => `
   <div class="input-group">
-    <input type="${type || 'text'}" id="${id}" value="${
-  value || ''
-}" placeholder="${placeholder || ''}">
+    <input 
+      type="${type || 'text'}"
+      id="${id}"
+      value="${value || ''}"
+      placeholder="${placeholder || ''}">
   </div>
 `
+
 const button = ({ id, label }) => `
   <button class='btn' id="${id}">${label}</button>
 `
@@ -18,25 +21,38 @@ const iframe = (link) => `
   <iframe style="display:none;" src="${link}" id="frame"></iframe>
 `
 $APP.innerHTML = `
-  ${input({ id: 'key', label: 'Key', placeholder: 'key', value:'' })}
-  ${input({ id: 'value', label: 'Value', placeholder: 'value', value:'' })}
+  ${input({ id: 'key', label: 'Key', placeholder: 'key', value: '' })}
+  ${input({ id: 'value', label: 'Value', placeholder: 'value', value: '' })}
   ${button({ id: 'btn', label: 'Send' })}
   ${iframe(URL_TO_SEND)}
   <a class='goto' href="${URL_TO_SEND}" target="_blank">Go to receiver</a>
 `
 
+const animateScale = (el, initScale=1.2, time=300) => {
+  el.style.transform = `scale(${initScale})`
+  setTimeout(() => {
+    el.style.transform = `scale(1)`
+    if (initScale > 1) {
+      setTimeout(() => animateScale(el, initScale - 0.05, time-50), time)
+    }
+  }, time)
+}
+
 // aqui viene la Magia
 
 const postCrossDomain = (data) =>
   document.getElementById('frame').contentWindow.postMessage(data, URL_TO_SEND)
-
-  const $key = document.getElementById('key')
-  const $value = document.getElementById('value')
+const $goto = document.querySelector('.goto')
+const $key = document.getElementById('key')
+const $value = document.getElementById('value')
 document.getElementById('btn').addEventListener('click', function () {
   var msg = {
     key: $key.value,
     value: $value.value,
   }
-  console.log('sending', {msg})
+  console.log('sending', { msg })
   postCrossDomain(msg)
+  $key.value = ''
+  $value.value = ''
+  animateScale($goto)
 })
